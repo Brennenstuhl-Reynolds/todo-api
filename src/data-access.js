c8a13fa11de08ada4ffff7c8f13e37ede940e04d
tensorchief <silvia.b.reynolds@gmail.com>
2021-09-25 11:08:52 +0200

    extended model data

--- src/data-access.js
+++ src/data-access.js
@@ -1,9 +1,37 @@
 let data = {
 	todos: [
-		{ id: 1, title: 'Einkaufen', version: 1 },
-		{ id: 2, title: 'Wäsche waschen', version: 1 },
-		{ id: 3, title: 'Wohnung aufräumen', version: 1 },
-		{ id: 4, title: 'Abfall leeren', version: 1 }
+		{ 	id: 1, // no due date + finished
+			title: 'Einkaufen',
+			description: 'Abfallsäcke',
+			importance: 5,
+			dueDate: '',
+			status: 1 ,
+			finishedDate: new Date(),
+			createdDate: new Date()	},
+		{ 	id: 2, // due next week
+			title: 'Wäsche waschen',
+			description: '60°, 30°',
+			importance: 5,
+			dueDate: new Date('2021-10-01'),
+			status: 1 ,
+			finishedDate: null,
+			createdDate: new Date()	},
+		{ 	id: 3, // due today
+			title: 'Wohnung aufräumen',
+			description: 'Besuch kommt!',
+			importance: 5,
+			dueDate: new Date(),
+			status: 1 ,
+			finishedDate: null,
+			createdDate: new Date()	},
+		{ 	id: 4, // Overdue
+			title: 'Abfall leeren',
+			description: 'Recycling zeugs nicht vergessen',
+			importance: 5,
+			dueDate: new Date('2021-09-01'),
+			status: 1 ,
+			finishedDate: null,
+			createdDate: new Date()	},
 	]
 }
 
