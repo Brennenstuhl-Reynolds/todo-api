5a76706615d054b5c4e50db44ac5d68158cf011f
tensorchief <silvia.b.reynolds@gmail.com>
2021-10-25 18:47:54 +0200

    code style cleanup

--- src/data-access.js
+++ src/data-access.js
@@ -1,71 +1,79 @@
 const { v4: uuidv4 } = require('uuid');
 
 let data = {
-	todos: [
-		{ 	id: "1", // no due date + finished
-			title: 'Einkaufen',
-			description: 'Abfallsäcke',
-			importance: 5,
-			dueDate: '',
-			status: 1 ,
-			finishedDate: new Date(),
-			createdDate: new Date()	},
-		{ 	id: "2", // due some time
-			title: 'Wäsche waschen',
-			description: '60°, 30°',
-			importance: 2,
-			dueDate: new Date('2021-10-20'),
-			status: 1 ,
-			finishedDate: null,
-			createdDate: new Date()	},
-		{ 	id: "3", // due today
-			title: 'Wohnung aufräumen',
-			description: 'Besuch kommt!',
-			importance: 5,
-			dueDate: new Date(),
-			status: 1 ,
-			finishedDate: null,
-			createdDate: new Date()	},
-		{ 	id: "4", // Overdue
-			title: 'Abfall leeren',
-			description: 'Recycling zeugs nicht vergessen',
-			importance: 4,
-			dueDate: new Date('2021-09-01'),
-			status: 1 ,
-			finishedDate: null,
-			createdDate: new Date()	},
-	]
-}
+    todos: [
+        {
+            id: 'c0b69d59-5f39-4a9e-8b9b-efb61e598762', // no due date + finished
+            title: 'Einkaufen',
+            description: 'Abfallsäcke',
+            importance: 5,
+            dueDate: '',
+            status: 1,
+            finishedDate: new Date(),
+            createdDate: new Date(),
+        },
+        {
+            id: '80862eab-dff7-4cfe-81e5-873a1786ec37', // due some time
+            title: 'Wäsche waschen',
+            description: '60°, 30°',
+            importance: 2,
+            dueDate: new Date('2021-11-10'),
+            status: 1,
+            finishedDate: null,
+            createdDate: new Date(),
+        },
+        {
+            id: 'a3e2811c-53e2-4e91-8e64-20424f98a46e', // due today
+            title: 'Wohnung aufräumen',
+            description: 'Besuch kommt!',
+            importance: 5,
+            dueDate: new Date(),
+            status: 1,
+            finishedDate: null,
+            createdDate: new Date(),
+        },
+        {
+            id: 'f5117531-67d4-4d65-86fc-4958c35b17dc', // Overdue
+            title: 'Abfall leeren',
+            description: 'Recycling zeugs nicht vergessen',
+            importance: 4,
+            dueDate: new Date('2021-09-01'),
+            status: 1,
+            finishedDate: null,
+            createdDate: new Date(),
+        },
+    ],
+};
 
 function get(entity) {
-	return data[entity]
+    return data[entity];
 }
 
 function find(entity, id) {
-	return data[entity].find(e => e.id === id)
+    return data[entity].find(e => e.id === id);
 }
 
 function insert(entity, row) {
-	row.id = uuidv4();
-	data[entity].push(row)
-	return row;
+    row.id = uuidv4();
+    data[entity].push(row);
+    return row;
 }
 
 function $delete(entity, id) {
-	const index = data[entity].indexOf(data[entity].find(e => e.id === id))
-	data[entity].splice(index, 1)
+    const index = data[entity].indexOf(data[entity].find(e => e.id === id));
+    data[entity].splice(index, 1);
 }
 
 function update(entity, id, row) {
-	const index = data[entity].indexOf(data[entity].find(e => e.id === id))
-	data[entity][index] = row;
-	return data[entity][index];
+    const index = data[entity].indexOf(data[entity].find(e => e.id === id));
+    data[entity][index] = row;
+    return data[entity][index];
 }
 
 module.exports = {
-	get,
-	find,
-	insert,
-	delete: $delete,
-	update
-}
+    get,
+    find,
+    insert,
+    delete: $delete,
+    update,
+};
--- src/index.js
+++ src/index.js
@@ -1,65 +1,65 @@
-const { get, find, insert, update, delete: $delete } = require('./data-access')
-const express = require('express')
-const bodyParser = require('body-parser')
-const app = express()
-const port = 3001
+const { get, find, insert, update, delete: $delete } = require('./data-access');
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+const port = 3001;
 
-app.use(bodyParser.json())
+app.use(bodyParser.json());
 
 app.get('/api/todos', (req, res) => {
-	res.send(get('todos'))
-})
+    res.send(get('todos'));
+});
 
 app.get('/api/todos/:id', (req, res) => {
-	const student = find('todos', req.params.id)
-	if (!student) {
-		res.statusCode = 404
-	}
-	res.send(student)
-})
+    const student = find('todos', req.params.id);
+    if (!student) {
+        res.statusCode = 404;
+    }
+    res.send(student);
+});
 
 app.post('/api/todos', (req, res) => {
 
-	if(!req.body.title) {
-		res.statusCode = 400;
-		res.send();
-		return;
-	}
+    if (!req.body.title) {
+        res.statusCode = 400;
+        res.send();
+        return;
+    }
 
-	const inserted = insert('todos', req.body)
-	res.statusCode = 204
-	res.send(inserted)
-})
+    const inserted = insert('todos', req.body);
+    res.statusCode = 204;
+    res.send(inserted);
+});
 
 app.put('/api/todos/:id', (req, res) => {
-	const student = find('todos', req.params.id)
-	if (!student) {
-		res.statusCode = 404;
-		res.send();
-		return;
-	}
+    const student = find('todos', req.params.id);
+    if (!student) {
+        res.statusCode = 404;
+        res.send();
+        return;
+    }
 
-	if(!req.body.title) {
-		res.statusCode = 400;
-		res.send();
-		return;
-	}
+    if (!req.body.title) {
+        res.statusCode = 400;
+        res.send();
+        return;
+    }
 
-	const updated = update('todos', req.params.id, req.body)
-	res.statusCode = 200
-	res.send(updated)
-})
+    const updated = update('todos', req.params.id, req.body);
+    res.statusCode = 200;
+    res.send(updated);
+});
 
 app.delete('/api/todos/:id', (req, res) => {
-	const student = find('todos', req.params.id)
-	if (!student) {
-		res.statusCode = 404
-	}
-	$delete('todos', req.params.id)
-	res.statusCode = 204
-	res.send()
-})
+    const student = find('todos', req.params.id);
+    if (!student) {
+        res.statusCode = 404;
+    }
+    $delete('todos', req.params.id);
+    res.statusCode = 204;
+    res.send();
+});
 
 app.listen(port, () => {
-	console.log(`App listening at http://localhost:${port}`)
-})
+    console.log(`App listening at http://localhost:${port}`);
+});
